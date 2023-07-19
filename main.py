ad552d9e960921e91ac6e6113d83a6177093a494
daorozc <daorozc@bancolombia.com.co>
2023-07-19 13:37:12 -0500

    refactor(main.py): encapsulate script in main function for better readability and maintainability

--- main.py
+++ main.py
@@ -7,26 +7,35 @@ from utils import get_repo_id, get_path_files, get_authorization_header
 from urls import create_url_repo_info, create_url_repo_items
 import os
 
-# Get the environment variables
-organization = os.getenv("ORGANIZATION")
-project = os.getenv("PROJECT")
-pat = os.getenv("PAT")
 
-# Set variables
-repo_name = "ISXXX0001"
-branch = "master"
-headers = get_authorization_header(pat)
+def main() -> None:
+    """
+    Entry function of the project
+    """
+    # Get the environment variables
+    organization = os.getenv("ORGANIZATION")
+    project = os.getenv("PROJECT")
+    pat = os.getenv("PAT")
 
-# Repo id
-url_repo_info = create_url_repo_info(organization, project, repo_name)
-repo_info = get_response(url_repo_info, headers)
-repo_id = get_repo_id(repo_info)
-print(f"\nRepository name = {repo_name}")
-print(f"Repostiry id = {repo_id}")
+    # Set variables
+    repo_name = "ISXXX0001"
+    branch = "master"
+    headers = get_authorization_header(pat)
 
-# Repo files
-url_repo_items = create_url_repo_items(organization, project, repo_id, branch)
-repo_items = get_response(url_repo_items, headers)
-files = get_path_files(repo_items)
-print(f"\nFiles number: {len(files)}")
-print(f"Files list: {files}")
+    # Repo id
+    url_repo_info = create_url_repo_info(organization, project, repo_name)
+    repo_info = get_response(url_repo_info, headers)
+    repo_id = get_repo_id(repo_info)
+    print(f"\nRepository name = {repo_name}")
+    print(f"Repostiry id = {repo_id}")
+
+    # Repo files
+    url_repo_items = create_url_repo_items(organization, project, repo_id, branch)
+    repo_items = get_response(url_repo_items, headers)
+    files = get_path_files(repo_items)
+    print(f"\nFiles number: {len(files)}")
+    print(f"Files list: {files}")
+
+
+if __name__ == "__main__":
+    main()
